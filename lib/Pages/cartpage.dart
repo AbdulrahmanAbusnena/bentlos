d6451f773c8e2c72ab344f3cc7f7c2fa10898c49
absuleahman_abusnea <user.name>
2024-03-17 08:43:13 +0000

    git commit

--- lib/Pages/cartpage.dart
+++ lib/Pages/cartpage.dart
@@ -1,5 +1,9 @@
+import 'package:bentlos/Components/listile2.dart';
+import 'package:bentlos/modules/product.dart';
+import 'package:bentlos/modules/shop.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -9,24 +13,46 @@ class CartPage extends StatefulWidget {
 }
 
 class CartPageState extends State<CartPage> {
+  void removeFromCart(Products products) {
+    Provider.of<EApp>(context, listen: false).removefromCart(products);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 5, 37, 64),
-      appBar: AppBar(
-        title: Text('Cart',
-            style: GoogleFonts.montserrat(
-              fontSize: 18,
-              fontWeight: FontWeight.w600,
-              color: Colors.white,
-            )),
-        centerTitle: true,
-        elevation: 0,
+    return Consumer<EApp>(builder: (context, value, child) {
+      return Scaffold(
         backgroundColor: const Color.fromARGB(255, 5, 37, 64),
-      ),
-      body: const Column(
-        children: [],
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Cart',
+              style: GoogleFonts.montserrat(
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              )),
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: const Color.fromARGB(255, 5, 37, 64),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: value.cart.length,
+                itemBuilder: ((context, index) {
+                  Products individualProducts = value.shop[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: ListTileCart(
+                      products: individualProducts,
+                      onTap: () => removeFromCart(individualProducts),
+                    ),
+                  );
+                }),
+              ),
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
